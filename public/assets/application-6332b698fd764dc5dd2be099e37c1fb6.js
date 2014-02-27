e0c5dca0310c12f1a7771b461db065efd455bc84
wudixiaotie <wudixiaotie@gmail.com>
2014-02-27 19:14:37 +0800

    Finish layout and routes

--- public/assets/application-6332b698fd764dc5dd2be099e37c1fb6.js
+++ public/assets/application-6332b698fd764dc5dd2be099e37c1fb6.js
@@ -10399,6 +10399,14 @@ if ( typeof define === "function" && define.amd && define.amd.jQuery ) {
   };
 
 }).call(this);
+(function() {
+
+
+}).call(this);
+(function() {
+
+
+}).call(this);
 // This is a manifest file that'll be compiled into application.js, which will include all the files
 // listed below.
 //
