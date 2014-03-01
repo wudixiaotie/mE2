b8f5cec378bc169a0eafa49d5dc76c69cea660d5
wudixiaotie <wudixiaotie@gmail.com>
2014-03-02 00:14:11 +0800

    Finish 7 test

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -4,24 +4,28 @@ describe UsersController do
 
 	subject { page }
 
-	# shared examples
-	shared_examples_for 'all user pages' do
-
-		it { should have_selector('h1', text: h1) }
-		it { should have_title(full_title(page_title)) }
-	end
-
   describe 'Sign up page' do
     before { visit signup_path }
 
-		it_should_behave_like 'all user pages'
-    let(:h1) { 'Sign up' }
-    let(:page_title) { 'Sign up' }
+    it { should have_title(full_title('Sign up')) }
+    it { should have_selector('h1', text: 'Sign up') }
     let(:submit) { "Create my account" }
 
     describe 'when input invalid info' do
     	it 'should not create a user' do
     	  expect { click_button submit }.not_to change(User, :count)
+      end
+
+      describe 'after submission' do
+        before { click_button submit }
+
+        it { should have_title(full_title('Sign up')) }
+        it { should have_content('Sign up') }
+        it { should have_content('* Name can\'t be blank') }
+        it { should have_content('* Email can\'t be blank') }
+        it { should have_content('* Email is invalid') }
+        it { should have_content('* Password can\'t be blank') }
+        it { should have_content('* Password is too short (minimum is 6 characters)') }
       end
     end
 
@@ -36,15 +40,22 @@ describe UsersController do
       it 'should create a user' do
     	  expect { click_button submit }.to change(User, :count).by(1)
     	end
+
+      describe 'after saving the user' do
+        before { click_button submit }
+        let(:user) { User.find_by(email: 'user@example.com') }
+
+        it { should have_title(user.name) }
+        it { should have_selector('div.alert.alert-success', text: 'Welcome') }
+      end
     end
   end
 
   describe 'profile page' do
   	before { visit user_path(user) }
 
-		it_should_behave_like 'all user pages'
   	let(:user) { FactoryGirl.create(:user) }
-    let(:h1) { user.name }
-    let(:page_title) { user.name }
+    it { should have_title(full_title(user.name)) }
+    it { should have_selector('h1', text: user.name) }
   end
 end
