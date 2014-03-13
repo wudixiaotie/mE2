0f6ff67edb572ba05f0487073b8abce6a97fb0f2
wudixiaotie <wudixiaotie@gmail.com>
2014-03-13 10:51:28 +0800

    Finish user edit, update, index, and destroy actions

--- public/assets/application-928f2f8d57e4a5c0f59c5ef06f13d3cd.js
+++ public/assets/application-928f2f8d57e4a5c0f59c5ef06f13d3cd.js
@@ -12355,22 +12355,6 @@ if ( typeof define === "function" && define.amd && define.amd.jQuery ) {
   };
 
 }).call(this);
-(function() {
-
-
-}).call(this);
-(function() {
-
-
-}).call(this);
-(function() {
-
-
-}).call(this);
-(function() {
-
-
-}).call(this);
 // This is a manifest file that'll be compiled into application.js, which will include all the files
 // listed below.
 //
